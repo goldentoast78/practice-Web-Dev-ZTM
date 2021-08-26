5e988c3ae99cb1ae187cc58eeb89900e2f818fe0
goldentoast78 <85243786+goldentoast78@users.noreply.github.com>
2021-08-26 21:21:34 +0800

    add npm lodash

--- bakgroundgenerate/script.js
+++ bakgroundgenerate/script.js
@@ -1,3 +1,8 @@
+var _ =require("lodash");
+var array =[1, 2, 3, 5, 6, 7, 8]
+console.log('answer: ',_.without(array, 5));
+
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
